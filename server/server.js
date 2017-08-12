e3567580cec8ba03f474150bbb810f0a6b182e21
Jin Choi <jinchoi@ucdavis.edu>
2017-08-11 17:57:46 -0700

    trying to get angular to load from server. readFile not reading file for some reason

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const fs = require('fs');
 
+const app = express();
 const port = process.env.PORT;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('/angular', (req, res) => {
+  fs.readFile(path.resolve(__dirname, '../node_modules/angular/angular.js'), 'utf8', angular => {
+    console.log(__dirname)
+    res.set('Content-Type', 'application/javascript').send(angular);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on mfing ${port}`);
