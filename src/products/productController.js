0f8cb26cf2085dec6ccc3bf6b5e523e17dd23d1e
Jin Choi <jinchoi@ucdavis.edu>
2017-08-21 14:41:12 -0700

    added widgets for image gallery with hover shadows. working on getting calc working for details

--- src/products/productController.js
+++ src/products/productController.js
@@ -1,40 +1,22 @@
 module.exports = app => {
   app.controller('ProductController', $scope => {
-  //probably going to load stuff from database, but that's later later
-  //probably going to make it more static than i think, because I want to match the images with the details
-  $scope.neonGrizzly = {
-    images: [],
-    summary: '',
-    details: [
-      {
-        imageUrl: '',
-        header: 'hi',
-        text: 'whats up dude'
-      },
-      {
-        header: 'hi',
-        text: 'whats up dude'
-      },
-      {
-        header: 'hi',
-        text: 'whats up dude'
-      },
-    ],
-  }
-
-  $scope.repeatMe = [
-    {
-        header: 'hi',
-        text: 'whats up dude'
-      },
-      {
-        header: 'hi',
-        text: 'whats up dude'
-      },
-      {
-        header: 'hi',
-        text: 'whats up dude'
-      },
-  ]
-})
+    //probably going to load stuff from database, but that's later later
+    //probably going to make it more static than i think, because I want to match the images with the details
+    $scope.neonGrizzly = {
+      imageUrls: [
+        'image0',
+        'image1',
+        'image2',
+        'image3',
+      ],
+      price: 150,
+      summary: 'HEEEEEERE I"M ALLOOOOOOWED EVERYTHING ALLLL OF THE TIIIIME',
+      details: [
+        'detail1',
+        'detail2',
+        'deatil3',
+        'detail4',
+      ],
+    }
+  })
 }
