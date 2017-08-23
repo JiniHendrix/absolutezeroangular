1e59d89554a9a60fc82ca5eb53baf82771db7feb
Jin Choi <jinchoi@ucdavis.edu>
2017-08-22 23:47:01 -0700

    working on submit function

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.static(path.resolve(__dirname, '../')));
 
 app.post('/email', (req, res) => {
   console.log(req.body);
+  //user nodemailer to send email to absolutezerobags@gmail.com
 })
 
 app.listen(port, () => {
--- src/contact/contactController.js
+++ src/contact/contactController.js
@@ -1,5 +1,19 @@
 module.exports = app => {
-  app.controller('Contact', ($scope) => {
-    
+  app.controller('ContactController', ($scope, $http) => {
+    //function that makes a post request using form values
+    //and then clears the input fields
+    //i could use this for my footer too since i dont have much to put in
+    //footer could also have join mailing list and social media icons that dont work? lol
+    $scope.email = e => {
+      console.log(e.target.childNodes)
+
+      const details = ['name', 'email', 'subject', 'body'];
+      const parsed = {};
+      details.forEach(detail => {
+        parsed[detail] = e.target.getElementById(detail).value
+      })
+
+      console.log(parsed);
+    }
   })
 }
