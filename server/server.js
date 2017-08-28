e4564764d804c438f1aec992d0242d77e3f89e3d
Jin Choi <jinchoi@ucdavis.edu>
2017-08-28 01:14:46 -0700

    serving from server

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-// app.use(express.static(path.resolve(__dirname, '../public')));
-// app.use(express.static(path.resolve(__dirname, '../')));
+app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, '../')));
 
 app.post('/email', emailController);
 
